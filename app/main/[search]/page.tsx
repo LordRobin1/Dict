b125a78880acb2aa157f065ff07a0fbcb698aad8
Robin Gebhardt <geb611018t@gmail.com>
2023-04-16 21:52:06 +0200

    definitions and quotes only update on submit

--- app/main/[search]/page.tsx
+++ app/main/[search]/page.tsx
@@ -3,29 +3,30 @@ import styles from '../../../styles/Quote.module.css';
 
 interface Props  {
     input: string,
+    search: string,
     quote: Quote
 }
 
-const Word = ({input, quote}: Props) => {
+const Word = ({search, input, quote}: Props) => {
     return (
         <div className={styles.wrapper}>
             <h2 className={styles.heading}>{input.charAt(0).toUpperCase() + input.slice(1)}</h2>
             <div className={styles.quote}>
-                {quote !== undefined && <Quote input={input} quote={quote}></Quote>}
+                {quote !== undefined && <Quote search={search} input={input} quote={quote}></Quote>}
             </div>
         </div>
     )
 }
 
 
-const Quote = ({input, quote}: Props) => {
+const Quote = ({search, quote}: Props) => {
     let key = 0
     let author = quote.char.name
     console.log(author)
     return(
         <div>
             {(quote.quote as string[]).map(str => {
-                if (str.toLowerCase() === input.toLowerCase()) {
+                if (str.toLowerCase() === search.toLowerCase()) {
                     key++
                     return <span style={{fontWeight: 'bold',fontSize: '20px', color: '#21cdef'}} key={key}>{str}</span>
                 } else {
--- app/main/page.tsx
+++ app/main/page.tsx
@@ -1,6 +1,6 @@
 'use client';
-import axios from 'axios';
 import { useState } from 'react';
+import { Data, wordDefs } from '../../code/definitions';
 import { Quote, quoteByKey } from '../../code/quotes';
 import styles from '../../styles/SearchBar.module.css';
 import Info from './[data]/page';
@@ -8,21 +8,22 @@ import Word from './[search]/page';
 
 const Main = () => {
     const [input, setInput] = useState<string>('')
+    const [search, setSearch] = useState<string>('')
     const [quote, setQuote] = useState<Quote>()
-
-    const [info, setInfo] = useState([]);
-    const wordURL = 'https://api.dictionaryapi.dev/api/v2/entries/en/'
+    const [info, setInfo] = useState<Data>([])
+    const [firstSearch, SetFirstSearch] = useState(true)
 
     const submit = async (e: any) => {
         e.preventDefault()
         if (input === '') return
-
+        if (firstSearch) SetFirstSearch(false)
+        
         let q = await quoteByKey(input)
         console.log(q)
         setQuote(q)
-        const res = await axios.get(`${wordURL}${input}`)
-        console.table(res.data)
-        checkDictRes(res.data) && setInfo(res.data)
+        const res = await wordDefs(input)
+        setInfo(res)
+        setSearch(input)
     }
 
     return (
@@ -31,14 +32,10 @@ const Main = () => {
                 <input className={styles.input} type="text" onChange={e => {setInput(e.target.value); console.log(input)}} placeholder='Search...'/>            
                 <button className={styles.button} type='submit'>Go!</button>
             </form>
-            <Word input={input} quote={quote as Quote}></Word>
-            <Info results={info}></Info>
+            <Word search={search} input={input} quote={quote as Quote}></Word>
+            <Info firstSearch={firstSearch} word={input} results={info}></Info>
         </div>
   )
 }
 
 export default Main
-
-const checkDictRes = (res: any) => {
-    return res.title === (null || undefined)
-}
